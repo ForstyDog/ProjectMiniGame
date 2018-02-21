37989bc92039cda17534bb665099844c361534b1
Niels Noordzij <315424@student.mboutrecht.nl>
2018-02-21 15:49:34 +0100

    Third commit

---
+++ Assets/_Scripts/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+	LevelManager levelmanager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		levelmanager.LoadNextLevel ();
+	}
+}
---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + Random.Range(1, 4));
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
